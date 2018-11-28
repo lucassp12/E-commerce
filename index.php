176f561fb505034692404692b94ddc631e43a95a
Lucas Santos <lucas_santos_pereira@hotmail.com>
2018-11-27 22:40:11 -0200

    Adiçao de Admim-categorias e seus templates

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ use \Hcode\PageAdmin;
 
 use \Hcode\Model\User;
 
+use \Hcode\Model\Category;
+
 
 
 $app = new Slim();
@@ -208,11 +210,14 @@ $app->post("/admin/forgot/reset", function(){
 	User::setForgotUsed($forgot["idrecovery"]);
 
 	$user = new User();
+
 	$user->get((int)$forgot["iduser"]);
 
-	$user->setPassword($_POST["password"]);
+	$password = password_hash("rasmuslerdorf", PASSWORD_DEFAULT, [
+		"const"=>12]
+	);
 
-	var_dump($_POST["password"]);
+	$user->setPassword($_POST["password"]);
 
 	$page = new PageAdmin([
 		"header"=>false,
@@ -220,6 +225,100 @@ $app->post("/admin/forgot/reset", function(){
 	]);
 	$page->setTpl("forgot-reset-success");
 });
+
+$app->get("/admin/categories", function(){
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		'categories'=>$categories
+
+	]);
+
+});
+
+
+$app->get("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory/delete",function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;
+
+
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update",array(
+
+		"category"=>$category->getValues()
+
+	));
+});
+
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+
+
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -214,5 +214,4 @@ class User extends Model{
 
 }
 
-
 ?>
