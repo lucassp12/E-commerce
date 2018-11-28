04da4757986e0b109b9cbb3adaa53b39e9249157
Lucas Santos <lucas_santos_pereira@hotmail.com>
2018-11-28 20:56:46 -0200

    Adicao de Admin-Produtos, e seus templates

---
+++ categories.php
@@ -0,0 +1,28 @@
+<?php
+use \Hcode\PageAdmin;
+
+use \Hcode\Model\User;
+
+use \Hcode\Model\Category;
+
+$app->get("/categories/:idcategory",function($idcategory){
+
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>[]
+	
+	]);
+
+});
+
+
+
+?>
